5e99604bff8fc5c251b6e6a6a02a94c527bc7c50
Scott Ruckh <github@gemneye.org>
2025-08-11 18:54:28 -0700

    chore: Add GOALS.md to gitignore and fix model download

--- runpod_handler.py
+++ runpod_handler.py
@@ -61,7 +61,8 @@ snapshot_download(
     repo_id="black-forest-labs/FLUX.1-dev",
     local_dir="{model_dir}",
     token=os.getenv("HF_TOKEN"),
-    local_dir_use_symlinks=False
+    resume_download=True,
+    max_retries=10
 )
 """
         subprocess.run([python_executable, "-c", download_script], check=True)
